900579184db931aae16d7b39a005bf2f0ee69213
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-08-16 09:42:56 +0530

    Update Dynamic_object.cpp
    Added program

--- Dynamic_object.cpp
+++ Dynamic_object.cpp
@@ -1,1 +1,28 @@
 
+#include<iostream>
+using namespace std;
+class Human
+{
+  public: 
+   string name;
+   int age;    
+  Human()
+  {
+    name = "Shivam";
+    age = 4;
+  }
+};
+
+int main()
+{
+  //Human obj;
+  //dynamically creating the object
+  
+  Human *obj = new Human();
+  cout<<obj->name<<endl;
+  cout<<obj->age<<endl;
+  cout<<"Sizeof "<<sizeof(obj->age);
+  delete obj;
+  return 0;
+  
+}
